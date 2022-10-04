44e7d2c9d90008ba69b33062ed810dedec980b73
Shazzana <shazzana.shahar@gmail.com>
2022-10-04 21:40:00 +0800

    First Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World</h1>
+        <p>you'll get a REACTion here</p>
+        <p>Will you???</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
